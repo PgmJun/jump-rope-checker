6c1544e376b7c0b90d6415a02a533a17513cfc9d
JUN <chltmdwns96@naver.com>
2023-03-07 16:20:57 +0900

    Project init

---
+++ src/main/java/com/jul/jumpropetornamentchecker/JumpRopeTornamentCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.jul.jumpropetornamentchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpRopeTornamentCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumpRopeTornamentCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jul/jumpropetornamentchecker/JumpRopeTornamentCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jul.jumpropetornamentchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpRopeTornamentCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
