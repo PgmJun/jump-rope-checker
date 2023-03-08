0cc984c0e7f1b77010e91f12eb76cb1c641d195f
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-03-08 22:44:29 +0900

    refactor: 스네이크 케이스, 카멜케이스 표기법으로 변경

--- src/main/java/com/jul/jumpropetornamentchecker/repository/CompetitionRepository.java
+++ src/main/java/com/jul/jumpropetornamentchecker/repository/CompetitionRepository.java
@@ -2,16 +2,17 @@ package com.jul.jumpropetornamentchecker.repository;
 
 import com.jul.jumpropetornamentchecker.domain.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
     Competition save(Competition competition);
 
-    List<Competition> findByCompetitionId(Long competition_id);
+    List<Competition> findByCompetitionId(Long competitionId);
 
-    List<Competition> findByCompetitionName(String competition_name);
+    List<Competition> findByCompetitionName(String competitionName);
 
     List<Competition> findAll();
 }
