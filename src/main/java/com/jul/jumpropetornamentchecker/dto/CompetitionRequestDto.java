2de5a5aae79fef16e98061f5e518bc9ae29b9410
JUN <chltmdwns96@naver.com>
2023-03-15 10:24:16 +0900

    fix: 대회 시작, 종료일 기록 자료형 변경
    LocalDate -> LocalDateTime

--- src/main/java/com/jul/jumpropetornamentchecker/dto/CompetitionRequestDto.java
+++ src/main/java/com/jul/jumpropetornamentchecker/dto/CompetitionRequestDto.java
@@ -3,11 +3,11 @@ package com.jul.jumpropetornamentchecker.dto;
 import com.jul.jumpropetornamentchecker.domain.Competition;
 import lombok.Builder;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Builder
 public record CompetitionRequestDto(String competitionName, String competitionHost, String hostEmail,
-                                    String hostTel, LocalDate competitionStartDate, LocalDate competitionEndDate) {
+                                    String hostTel, LocalDateTime competitionStartDate, LocalDateTime competitionEndDate) {
 
     public Competition to() {
         return Competition.builder()
