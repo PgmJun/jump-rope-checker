92c2290c063fdfb9ea30dbd48e61fe99c9920f74
JUN <chltmdwns96@naver.com>
2023-03-15 10:28:35 +0900

    fix: 기록지 인쇄명 컬럼 추가

--- src/main/java/com/jul/jumpropetornamentchecker/dto/CompetitionRequestDto.java
+++ src/main/java/com/jul/jumpropetornamentchecker/dto/CompetitionRequestDto.java
@@ -6,12 +6,15 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 
 @Builder
-public record CompetitionRequestDto(String competitionName, String competitionHost, String hostEmail,
-                                    String hostTel, LocalDateTime competitionStartDate, LocalDateTime competitionEndDate) {
+public record CompetitionRequestDto(String competitionName, String recordingSheetName, String competitionHost,
+                                    String hostEmail,
+                                    String hostTel, LocalDateTime competitionStartDate,
+                                    LocalDateTime competitionEndDate) {
 
     public Competition to() {
         return Competition.builder()
                 .competitionName(competitionName())
+                .recordingSheetName(recordingSheetName())
                 .competitionHost(competitionHost())
                 .hostEmail(hostEmail())
                 .hostTel(hostTel())
