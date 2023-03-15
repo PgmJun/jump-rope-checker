d226edc1ca06979d1fb86da3a3114bd998cd41bd
JUN <chltmdwns96@naver.com>
2023-03-15 10:49:46 +0900

    feat: Controller Operation 적용 #6

--- src/main/java/com/jul/jumpropetornamentchecker/api/CompetitionController.java
+++ src/main/java/com/jul/jumpropetornamentchecker/api/CompetitionController.java
@@ -4,7 +4,7 @@ import com.jul.jumpropetornamentchecker.dto.CompetitionRequestDto;
 import com.jul.jumpropetornamentchecker.dto.CompetitionResponseDto;
 import com.jul.jumpropetornamentchecker.dto.CompetitionUpdateDto;
 import com.jul.jumpropetornamentchecker.service.CompetitionService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +20,7 @@ public class CompetitionController {
     private final CompetitionService competitionService;
 
     @PostMapping("/add")
+    @Operation(summary = "대회 추가 API", description = "대회 정보를 입력하여 대회를 추가합니다.")
     public ResponseEntity<?> registerCompetitionData(@RequestBody CompetitionRequestDto competitionDto) {
         Boolean saveResult = competitionService.saveCompetition(competitionDto);
 
@@ -29,6 +30,7 @@ public class CompetitionController {
     }
 
     @GetMapping("/find/all")
+    @Operation(summary = "대회 전체 조회 API", description = "전체 대회 정보를 조회합니다.")
     public ResponseEntity<?> findAllCompetitionData() {
         List<CompetitionResponseDto> competitionInfo = competitionService.findAllCompetitionInfo();
         return (competitionInfo.isEmpty()) ?
@@ -37,6 +39,7 @@ public class CompetitionController {
     }
 
     @GetMapping("/find")
+    @Operation(summary = "대회 이름 조회 API", description = "대회명을 사용하여 대회 정보를 조회합니다.")
     public ResponseEntity<?> findCompetitionDataByName(@RequestParam("name") String competitionName) {
         List<CompetitionResponseDto> competitionInfo = competitionService.findCompetitionInfoByName(competitionName);
         return (competitionInfo.isEmpty()) ?
@@ -45,6 +48,7 @@ public class CompetitionController {
     }
 
     @GetMapping("/find/{id}")
+    @Operation(summary = "대회 ID 조회 API", description = "대회ID를 사용하여 대회 정보를 조회합니다.")
     public ResponseEntity<?> findCompetitionDataByName(@PathVariable("id") Long competitionId) {
         Optional<CompetitionResponseDto> competitionInfo = competitionService.findCompetitionInfoById(competitionId);
         return (competitionInfo.isEmpty()) ?
@@ -53,6 +57,7 @@ public class CompetitionController {
     }
 
     @DeleteMapping("/delete")
+    @Operation(summary = "대회 정보 삭제 API", description = "대회ID List를 통해 대회 정보를 삭제합니다.")
     public ResponseEntity<?> deleteCompetitionDataById(@RequestBody List<Long> competitionIds) {
         return (competitionService.removeCompetitionData(competitionIds)) ?
                 new ResponseEntity<>("competition datum are removed", HttpStatus.OK) :
@@ -60,9 +65,10 @@ public class CompetitionController {
     }
 
     @PutMapping("/update")
+    @Operation(summary = "대회 정보 수정 API", description = "대회 정보를 업데이트합니다.")
     public ResponseEntity<?> updateCompetitionData(@RequestBody CompetitionUpdateDto competitionUpdateDtos) {
         return (competitionService.updateCompetitionData(competitionUpdateDtos)) ?
-                new ResponseEntity<>("competition datum is updated",HttpStatus.OK) :
+                new ResponseEntity<>("competition datum is updated", HttpStatus.OK) :
                 new ResponseEntity<>("competition datum are fail to update", HttpStatus.BAD_REQUEST);
     }
 
